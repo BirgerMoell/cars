7e575835cc9d2f583307fd681c6f5dd14052fe25
Birger Moëll <birger.moell@gmail.com>
2017-04-27 09:40:28 +0200

    current progress

--- src/data/api.js
+++ src/data/api.js
@@ -9,6 +9,7 @@ export default class Api extends React.Component {
     this.setFilter = this.setFilter.bind(this);
   }
 
+  // calls when the input field is changed
   handleChange(event) {
     var filterValue = document.getElementById("filterInput").value
     this.setState({filter: filterValue});
@@ -18,68 +19,59 @@ export default class Api extends React.Component {
     }
   }
 
-  componentDidMount() {
+  componentWillMount() {
     fetch('http://localhost:3000/cars.json')
       .then(result=>result.json())
       .then(cars=>this.setState({cars}))
+      //console.log("the component did mount");
+      //console.log(this.state);
+      this.setState({filteredCars: this.state.cars})
+      //let filterValue = document.getElementById("filterInput").value
+
+      //this.setState({filter: filterValue})
+  }
+
+  componentDidMount() {
     this.setState({filteredCars: this.state.cars})
   }
 
   setFilter() {
-    if (this.state.filter.length === 0) {
-      return "filteredCars"
-    }
-    else {
-      return "cars"
+    if (document.getElementById("filterInput").value === 0) {
+      this.setState({filteredCars: this.state.cars})
     }
   }
 
 
-  render() {
 
+  render() {
 
     return(
       <div>
-
         <div className="car-list">
-
           <label>
             <h5>Filter year</h5>
             <input type="text" id="filterInput" value={this.state.value} onChange={this.handleChange} />
             <p id="filter"></p>
           </label>
 
-
-          {this.state.filteredCars.map(cars=>
-
+          {
+            //this.state.cars.filter(cars => cars.year === parseInt(filterValue)).map(cars=>
+            this.state.filteredCars.map(cars =>
             <div
-              className="car"
-              key={cars.reg}
-              style={{borderColor: cars.color}}>
+             className="car"
+             key={cars.reg}
+             style={{borderColor: cars.color}}>
+             <p key={cars.id}>{cars.reg} {cars.name} {cars.year}</p>
 
-            <p key={cars.id}>{cars.reg} {cars.name} {cars.year}</p>
+           </div>
+
+             )}
+
 
             </div>
 
-
-
-            )}
-
-
-      </div>
-
-
-
-
-  </div>
+          </div>
 
     );
   }
 }
-
-/*
-"reg": "KAK313",
-"name": "Volvo",
-"color": "#00ffff",
-"year": 2002
-*/
